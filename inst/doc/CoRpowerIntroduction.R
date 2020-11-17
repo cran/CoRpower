3cd5f650a86d1b61d213d1f4c70c297d4c9567c6
Michal Juraska <mjuraska@fredhutch.org>
2020-11-17 07:10:14 +0000

    version 1.0.4

--- inst/doc/CoRpowerIntroduction.R
+++ inst/doc/CoRpowerIntroduction.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(CoRpower)
 computeN(Nrand = 4100,          # participants randomized to vaccine arm
          tau = 3.5,             # biomarker sampling timepoint
@@ -9,7 +9,7 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
          dropoutRisk = 0.1,     # dropout risk between 0 and 'taumax'
          propCasesWithS = 1)    # proportion of observed cases with measured S(1)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -27,17 +27,17 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,  # 'computePower' output list of lists
 #               legendText = paste0("Control:Case = ", c("5:1", "3:1", "1:1")))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  computePower(..., saveDir = "myDir", saveFile = c("myFile1.RData", "myFile2.RData", "myFile3.RData"))
 #  plotPowerTri(outComputePower = c("myFile1.RData", "myFile2.RData", "myFile3.RData"), # 'computePower' output files
 #               outDir = rep("~/myDir", 3),                           # path to each myFilex.RData
 #               legendText = paste0("Control:Case = ", c("5:1", "3:1", "1:1")))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -56,11 +56,11 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,
 #               legendText = paste0("Sens = Spec = ", c(1, 0.9, 0.8, 0.7)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -78,14 +78,14 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,
 #               legendText = c("Plat0=0.05, Plat2=0.15",
 #                              "Plat0=0.1, Plat2=0.3",
 #                              "Plat0=0.15, Plat2=0.45",
 #                              "Plat0=0.2, Plat2=0.6"))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -104,28 +104,28 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,
 #               legendText = paste0("rho = ", c(1, 0.9, 0.7, 0.5)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotRRgradVE(outComputePower = pwr,  # 'computePower' output list of lists
 #               legendText = paste0("rho = ", c(1, 0.9, 0.7, 0.5)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  computePower(..., saveDir = "myDir", saveFile = "myFile.RData")
 #  plotRRgradVE(outComputePower = paste0("myFile_rho_", c(1, 0.9, 0.7, 0.5), ".RData"),    # files with 'computePower' output
 #               outDir = "~/myDir",            # path to myFile.RData
 #               legendText = paste0("rho = ", c(1, 0.9, 0.7, 0.5)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotROCcurveTri(Plat0 = 0.2,
 #                  Plat2 = c(0.2, 0.3, 0.4, 0.5),
 #                  P0 = seq(0.90, 0.10, len=25),
 #                  P2 = seq(0.10, 0.90, len=25),
 #                  rho = c(1, 0.9, 0.7, 0.5))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -144,14 +144,14 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,
 #               legendText = c("Plat0=0.05, Plat2=0.15",
 #                              "Plat0=0.1, Plat2=0.3",
 #                              "Plat0=0.15, Plat2=0.45",
 #                              "Plat0=0.2, Plat2=0.6"))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = c(25, 32, 35, 40),
 #                      nControlsTx = c(3661, 3654, 3651, 3646),
 #                      nCasesTxWithS = c(25, 32, 35, 40),
@@ -170,11 +170,11 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,
 #               legendText = paste0("nCasesTx = ", c(25, 32, 35, 40)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = c(25, 32, 35, 40),
 #                      nControlsTx = c(3661, 3654, 3651, 3646),
 #                      nCasesTxWithS = c(25, 32, 35, 40),
@@ -193,11 +193,11 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "binary")          # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,
 #               legendText = paste0("nCasesTx = ", c(25, 32, 35, 40)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -212,25 +212,25 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                # two-sided Wald test Type 1 error rate
 #                      biomType = "continuous")     # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerCont(outComputePower = pwr,          # output list of lists from 'computePower'
 #                legendText = paste0("rho = ", c(1, 0.9, 0.7, 0.5)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  computePower(..., saveDir = "myDir", saveFile = "myFile.RData")
 #  plotPowerCont(outComputePower = paste0("myFile_rho_", c(1, 0.9, 0.7, 0.5), ".RData"),     # files with 'computePower' output
 #                outDir = "~/myDir",             # path to myFile.RData
 #                legendText = paste0("rho = ", c(1, 0.9, 0.7, 0.5)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotVElatCont(outComputePower = pwr)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  computePower(..., saveDir = "myDir", saveFile = "myFile.RData")
 #  plotVElatCont(outComputePower = "myFile.RData",
 #                outDir = "~/myDir")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -245,11 +245,11 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                # two-sided Wald test Type 1 error rate
 #                      biomType = "continuous")     # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerCont(outComputePower = pwr,          # output list of lists from 'computePower'
 #                legendText = paste0("PlatVElowest = ", c(0.05, 0.1, 0.15, 0.2)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -268,11 +268,11 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                 # two-sided Wald test Type 1 error rate
 #                      biomType = "trichotomous")    # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerTri(outComputePower = pwr,  # 'computePower' output
 #               legendText = paste0("Cohort p = ", c(0.01, 0.02, 0.03, 0.05)))
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  pwr <- computePower(nCasesTx = 32,
 #                      nControlsTx = 3654,
 #                      nCasesTxWithS = 32,
@@ -288,7 +288,7 @@ computeN(Nrand = 4100,          # participants randomized to vaccine arm
 #                      alpha = 0.05,                # two-sided Wald test Type 1 error rate
 #                      biomType = "continuous")     # "continuous" by default
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  plotPowerCont(outComputePower = pwr,  # 'computePower' output
 #                legendText = paste0("Cohort p = ", c(0.01, 0.02, 0.03, 0.05)))
 
